565e380b2c069a7fcaa2d05b1dc17079485b8a7c
camiDelaossa <maria.camila211275@gmail.com>
2024-10-13 17:05:06 -0500

    update NavigateToTextBoxTask

--- src/main/java/tasks/NavigateToTextBoxTask.java
+++ src/main/java/tasks/NavigateToTextBoxTask.java
@@ -5,6 +5,7 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Scroll;
 import userInterface.TextBoxUI;
 
 public class NavigateToTextBoxTask implements Task {
@@ -12,6 +13,7 @@ public class NavigateToTextBoxTask implements Task {
   @Override
   public <T extends Actor> void performAs(T actor) {
     actor.attemptsTo(
+        Scroll.to(TextBoxUI.BTN_ELEMENTS),
         Click.on(TextBoxUI.BTN_ELEMENTS),
         Click.on(TextBoxUI.BTN_TEXT_BOX)
     );
